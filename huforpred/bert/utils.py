705ce2329196490f0f407a2ed5b46c3bae43bb08
papasebestyen <pap.sebestyen@gmail.com>
2022-05-22 13:42:39 +0000

    add text notebooks

---
+++ huforpred/bert/utils.py
@@ -0,0 +1,10 @@
+from datasets import load_metric
+import numpy as np
+
+metric = load_metric("f1")
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    acc = metric.compute(predictions=predictions, references=labels)
+    return acc
